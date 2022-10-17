b1dc92b66b92c6ab7e4d0bc43e3089b5057613b7
Matt Thompson <mattthomp12@gmail.com>
2022-10-17 12:54:39 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Matthew Thompson Soto on 10/17/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
